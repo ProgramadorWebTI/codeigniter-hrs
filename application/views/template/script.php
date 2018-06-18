0fe5bfca3ea5d6256c351593de3eb36495ff0014
ProgramadorWebTI <programadorwebti@gmail.com>
2018-06-18 01:34:56 -0300

    Projeto em branco do codeigniter

---
+++ application/views/template/script.php
@@ -0,0 +1,11 @@
+<!-- ===== LIBIS JAVA SCRIPT ===== -->
+<script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/lib/alertifyjs/alertify.min.js'); ?>"></script>
+
+<!--LIBIS CSS -->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/lib/alertifyjs/css/alertify.min.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/lib/alertifyjs/css/themes/bootstrap.min.css'); ?>">
+
+
